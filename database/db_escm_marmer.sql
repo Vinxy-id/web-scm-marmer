0527932f462638aad17cc255a7ace3abaf86c5b4
Alvin <alvinwidiantara2019@gmail.com>
2026-08-22 20:54:40 +0700

    fix: clean SQL dump syntax for 100% TiDB Cloud compatibility

--- database/db_escm_marmer.sql
+++ database/db_escm_marmer.sql
@@ -1,25 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Aug 22, 2026 at 06:33 AM
--- Server version: 8.4.3
--- PHP Version: 8.3.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_escm_marmer`
---
+-- E-SCM Marmer Tulungagung SQL Schema & Seed Data
+-- Compatible with MySQL 8.0+ and TiDB Cloud Serverless
+-- --------------------------------------------------------
 
 -- --------------------------------------------------------
 
@@ -748,8 +729,4 @@ ALTER TABLE `work_orders`
   ADD CONSTRAINT `fk_wo_creator` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_wo_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_wo_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
